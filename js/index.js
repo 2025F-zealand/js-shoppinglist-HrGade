ccca8d57a3ece98676b88314ff78c5498a98cb8d
Gade <adamgadefrimann@gmail.com>
2025-03-25 21:45:45 +0100

    added task 2:

--- js/index.js
+++ js/index.js
@@ -1,30 +1,36 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
 
-function createListItem(className, id, text)
-{
-    //Lav li-element
-    let li = createElement("li");
+document.addEventListener("DOMContentLoaded", function () {
+    function createListItem(className, id, text) {
+        let li = document.createElement("li");
 
+        // Sæt attributter
+        li.setAttribute("class", className);
+        li.setAttribute("id", id);
 
-    //set classname
-    li.className = className
-    li.setAttribute("class", className);
+        // Opret en tekstnode og tilføj den til li
+        let textNode = document.createTextNode(text);
+        li.appendChild(textNode);
 
-    //set id
-    li.id = id;
-    li.setAttribute("id", id);
+        return li;
+    }
 
-    //lav en tekst-note og forbind den til en variabel textNode
-    li.textContent = text;
-    let textNode = document.createTextNode(text);
+    function appendListItem(listId, listItem) {
+        let list = document.getElementById(listId);
 
-    //Append her tekst-noten til "li-elementet"
-    li.appendChild(textNode);
-    
-    return li;
-}
+        if (list) {
+            list.appendChild(listItem);
+            console.log(`Item added to ${listId}:`, listItem); 
+        } else {
+            console.error(`List with id '${listId}' not found.`);
+        }
+    }
 
-let newItem = createListItem("list-item", "item1", "Milk");
+    // **Test om det virker**
+    let newUnhealthyItem = createListItem("unhealthy", "5", "Chocolate");
+    appendListItem("listUnhealthy", newUnhealthyItem);
 
-console.log(newItem);
+    let newHealthyItem = createListItem("healthy", "6", "Bananas");
+    appendListItem("list", newHealthyItem);
+});
