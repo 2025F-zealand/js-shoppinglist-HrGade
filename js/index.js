53beb1caa29699f33f535beab39cb2f183f5dcb7
Gade <adamgadefrimann@gmail.com>
2025-03-25 21:36:42 +0100

    added task 1

--- js/index.js
+++ js/index.js
@@ -1,2 +1,30 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+function createListItem(className, id, text)
+{
+    //Lav li-element
+    let li = createElement("li");
+
+
+    //set classname
+    li.className = className
+    li.setAttribute("class", className);
+
+    //set id
+    li.id = id;
+    li.setAttribute("id", id);
+
+    //lav en tekst-note og forbind den til en variabel textNode
+    li.textContent = text;
+    let textNode = document.createTextNode(text);
+
+    //Append her tekst-noten til "li-elementet"
+    li.appendChild(textNode);
+    
+    return li;
+}
+
+let newItem = createListItem("list-item", "item1", "Milk");
+
+console.log(newItem);
